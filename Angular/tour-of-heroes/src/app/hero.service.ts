002b33d7629390f221851ed5f06251fa7b09778f
Prado Bognot <pbognot.ch@gmail.com>
2023-06-12 13:26:47 +0800

    Angular Tour of Heroes - Message Service implementation

--- Angular/tour-of-heroes/src/app/hero.service.ts
+++ Angular/tour-of-heroes/src/app/hero.service.ts
@@ -4,19 +4,20 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
-
-  // getHeroes(): Hero[] {
-  //   return HEROES;
-  // }
+  constructor(
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
--- Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,32 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-
-import { HEROES } from '../mock-heroes';
-
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent {
-  heroes: Hero[] = [];
-  selectedHero?: Hero;
+export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) {}
+  selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  getHeroes(): void {
-    // this.heroes = this.heroService.getHeroes();
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
