afb43f3a6bbe096c2ff581d4b3da75a68186cf3d
Prado Bognot <pbognot.ch@gmail.com>
2023-06-12 12:21:25 +0800

    Angular Tour of Heroes + Create Hero Service for accessing data

--- Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -3,19 +3,26 @@ import { Hero } from '../hero';
 
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
-  heroes = HEROES;
-  selectedHero?: Hero;
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
