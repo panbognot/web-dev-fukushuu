295b1fb00956e05c36d871457fce260196d82022
Prado Bognot <pbognot.ch@gmail.com>
2023-06-14 10:26:57 +0800

    Angular tour of heroes tutorial + Create structure for in memory data service - Setup in memory data service on app module

--- Angular/tour-of-heroes/src/app/app.module.ts
+++ Angular/tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -20,6 +24,10 @@ import { MessagesComponent } from './messages/messages.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
     AppRoutingModule
   ],
   providers: [],
