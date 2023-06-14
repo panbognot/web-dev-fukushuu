692f41719673146d9c0a1673e2ec49ece4802319
Prado Bognot <pbognot.ch@gmail.com>
2023-06-14 12:24:59 +0800

    Angular tour of heroes tutorial - Hero Service: 1. Create private log function that use the message service for brevity 2. Use http get for getHeroes() function

--- Angular/tour-of-heroes/src/app/hero.service.ts
+++ Angular/tour-of-heroes/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
@@ -11,22 +12,30 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes'; // URL to web api
+
   constructor(
+    private http: HttpClient,
     private messageService: MessageService
   ) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    this.log(`fetched heroes`);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified 'id' always exists.
     // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    // this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  // Log a HeroService message with the MesssageService
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
