7863c0f07f37a179ec50a7a0f9365f3c9d340fd6
Rad Bognot <pbognot.ch@gmail.com>
2023-07-31 12:21:29 +0800

    AtBS w/ Python: Chapter 09 - Updated "Mad Libs"

--- Python/PyAutomateBoringStuff/ch09_readingWritingFiles/Quizzes/madLibs.py
+++ Python/PyAutomateBoringStuff/ch09_readingWritingFiles/Quizzes/madLibs.py
@@ -2,32 +2,46 @@
 # their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears 
 # in the text file. 
 from pathlib import Path
+import sys
+import pyinputplus as pyip
 
-p = Path('ch09_readingWritingFiles/Quizzes')
+# p = Path('ch09_readingWritingFiles/Quizzes')
+p = Path()
+
+# Open a specific file template if there is a user input
+# Usage: py madLibs.py <file name>
+fileName = 'base.txt'
+if len(sys.argv) == 2:
+  fileName = sys.argv[1]
 
 # Replace this later on with argv style to access ANY text file
-fd = open(p / 'base.txt')
+fd = open(p / fileName)
 baseContent = fd.readlines()
 fd.close()
 
 ctr = 0
+print(str(baseContent) + '\n')
 for line in baseContent:
   lineSplit = line.split()
 
   i = 0
   for word in lineSplit:
-    # TODO: Ask user for input if it is an ADJECTIVE
+    # Ask user for input if it is an ADJECTIVE
     if "ADJECTIVE" in word:
-      lineSplit[i] = word.replace("ADJECTIVE", "random adjective")
-    # TODO: Ask user for input if it is an NOUN
+      inAdj = pyip.inputStr('Enter an adjective:\n')
+      lineSplit[i] = word.replace("ADJECTIVE", inAdj)
+    # Ask user for input if it is an NOUN
     elif "NOUN" in word:
-      lineSplit[i] = word.replace("NOUN", "random noun")
-    # TODO: Ask user for input if it is an ADVERB
+      inNoun = pyip.inputStr('Enter a noun:\n')
+      lineSplit[i] = word.replace("NOUN", inNoun)
+    # Ask user for input if it is an ADVERB
     elif "ADVERB" in word:
-      lineSplit[i] = word.replace("ADVERB", "random adverb")
-    # TODO: Ask user for input if it is an VERB
+      inAdverb = pyip.inputStr('Enter an adverb:\n')
+      lineSplit[i] = word.replace("ADVERB", inAdverb)
+    # Ask user for input if it is an VERB
     elif "VERB" in word:
-      lineSplit[i] = word.replace("VERB", "random verb")
+      inVerb = pyip.inputStr('Enter a verb:\n')
+      lineSplit[i] = word.replace("VERB", inVerb)
 
     i += 1
 
