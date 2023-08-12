b5fb57196ecd4afd58039ef23c12b220f78b44f4
Prado Bognot <pbognot.ch@gmail.com>
2023-08-12 12:35:08 +0800

    AtBS ch 10: - Copy a folder

--- Python/PyAutomateBoringStuff/ch10_organizingFiles/01_copyingFilesFolders.py
+++ Python/PyAutomateBoringStuff/ch10_organizingFiles/01_copyingFilesFolders.py
@@ -2,5 +2,11 @@ import shutil, os
 from pathlib import Path
 
 p = Path.cwd() / 'ch10_organizingFiles'
+
+# Copy the file into a folder and retain the filename
 shutil.copy(p / 'pets.txt', p / 'test_folder')
+# Copy the file and rename it
 shutil.copy(p / 'kuma.txt', p / 'test_folder/cute.txt')
+
+# Copy a whole folder
+shutil.copytree(p / 'test_folder', p / 'bkp_folder')
