619ea49ff77586edbc7046ebf004c6625e3f15b5
Prado Bognot <pbognot.ch@gmail.com>
2023-08-17 19:20:23 +0800

    ATBS with Python: ch 10 - The creation of ZIP files does NOT work straightforward on a folder structure

--- Python/PyAutomateBoringStuff/ch10_organizingFiles/08_createAddZIPfiles.py
+++ Python/PyAutomateBoringStuff/ch10_organizingFiles/08_createAddZIPfiles.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 p = Path.cwd() / 'ch10_organizingFiles'
 
+# NOTE: This DOES NOT zip an entire folder structure...
 newZip = zipfile.ZipFile(p / 'new.zip', 'w')
 newZip.write(p / 'cuties', compress_type=zipfile.ZIP_DEFLATED)
 newZip.close()
