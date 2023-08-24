e357afe42e6365820ae3be04c85a3ec3532f6199
Rad Bognot <pbognot.ch@gmail.com>
2023-08-24 18:25:07 +0800

    ATBS with Python: ch 12 - Web Scraping + Saving downloaded files to the hard drive

---
+++ Python/PyAutomateBoringStuff/ch12_webScraping/04_saveDLfiles.py
@@ -0,0 +1,12 @@
+# Downloads the text file for Romeo and Juliet from a website
+
+import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+
+for chunk in res.iter_content(100000):
+  playFile.write(chunk)
+
+playFile.close()
